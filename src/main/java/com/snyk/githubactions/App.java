1c00a62bc58e34dbac13e3080b6d1077270c96f6
clarkio <clarkio@users.noreply.github.com>
2024-02-27 14:47:28 -0500

    init with blog source

---
+++ src/main/java/com/snyk/githubactions/App.java
@@ -0,0 +1,13 @@
+package com.snyk.githubactions;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
